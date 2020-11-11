756e0ef22dd4f6a3e9609333ac1ffd4739e459be
Viet Dung <dungtv09@gmail.com>
2020-11-11 15:13:35 +0700

    add font awesome

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,18 +9,26 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      // google font
       {
         href:
           'https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@0,200;0,300;0,400;0,600;0,700;0,800;0,900;1,200;1,300;1,400;1,600;1,700;1,800;1,900&display=swap',
         rel: 'stylesheet',
       },
+      // bootstrap
       {
         rel: 'stylesheet',
         href:
           'https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css',
       },
+      // font awesome
+      {
+        rel: 'stylesheet',
+        href: 'https://use.fontawesome.com/releases/v5.15.1/css/all.css',
+      },
     ],
     script: [
+      // bootstrap
       { src: 'https://code.jquery.com/jquery-3.5.1.slim.min.js' },
       {
         src:
